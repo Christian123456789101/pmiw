0a3c2a9b0eb29fec831c4d94fadc23172feb4315
Christian123456789101 <166933386+Christian123456789101@users.noreply.github.com>
2025-09-13 10:23:45 -0300

    Add files via upload

---
+++ tp1/funciones.js
@@ -0,0 +1,36 @@
+
+function esPar(tec) {
+  return tec % 2 === 0;
+}
+
+function teclaColor() {
+  return esPar(tec);
+}
+
+function grilla() {
+  for (let x = 400; x < width; x += cant) {
+    for (let y = 0; y < height; y += cant) {
+      let distancia = dist(mouseX, 0, x, 0);
+      let tono = dist(width / 50, height / 5, 0, 0) / distancia * 255;
+      
+      tam = dist(width, height / 2, 0, 0) / distancia;
+      if (tam > 24) tam = 24;
+      if (tam < 15) tam = 15;
+      
+      if (mouseIsPressed || clicks % 2 != 0) { 
+        if (teclaColor()) {
+          fill(255);
+        } else {
+          fill(0);
+        }
+      } else {
+        if (teclaColor()) {
+          fill(255, tono);
+        } else {
+          fill(0, tono);
+        }
+      }
+      ellipse(x, y, tam, tam);
+    }
+  }
+}
---
+++ tp1/tp1.js
@@ -0,0 +1,44 @@
+//Christian Strack
+//119149/4
+//comisión 2
+//tp1
+//PMIW
+
+let tam = 23;
+let cant = 25;
+let tec = 0;
+let num = 0;
+let clicks = 0;
+
+function preload() {
+  imagen = loadImage("data/F_17.jpg");
+}
+
+function setup() {
+  createCanvas(800, 400);
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+    tec++;
+  }
+}
+
+function mouseClicked() {
+  clicks++
+}
+
+function draw() {
+  noStroke();
+  
+  if (clicks % 2 == 0) {
+    if (teclaColor()) {
+      background(0);
+    } else {
+      background(255);
+    }
+  }
+
+  grilla();
+  image(imagen, 0, 0, 400, 400);
+}
