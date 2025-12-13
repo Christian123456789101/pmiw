58ed7cbfff4ec43cee5fbb6dff9471efa65b7f35
Christian123456789101 <166933386+Christian123456789101@users.noreply.github.com>
2025-12-13 16:58:15 -0300

    Update tpfinalparte2.js: video
    se agrego el video

--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -1,5 +1,5 @@
 // christian strack y carmela urturi
-//falt video
+//video: https://youtu.be/QflMXCUrYMo
 let fondo;
 let juego;
 let imgJugador;
@@ -23,3 +23,4 @@ function setup() {
 function draw() {
   juego.actualizar();
 }
+
